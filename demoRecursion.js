8102ebf0d1b89c25e9da04876bf825942a48cf4c
Sean Luciw <131205783+seanluciw@users.noreply.github.com>
2023-05-10 13:37:42 -0700

    Update demoRecursion.js
    added text

--- demoRecursion.js
+++ demoRecursion.js
@@ -12,9 +12,10 @@ function functRecursion(n) {
 }
 
 function functDemoRecursion(num) {
+	console.log("welcome to my little program showing recursion with a metaphor.");
 	arr = functRecursion(num);
 	console.log("recursion created array: " + arr);
-	console.log("this program is meant to illuminate the nature of recursion. i realized it's like winding up a spool of thread and then, after base case is reached, the spool unwinds.\n\n")
+	console.log("this program is meant to illuminate the nature of recursion. i realized it's like winding up a spool of thread and then, after base case is reached, the spool unwinds. The array is created during the unwinding phase. \n\n")
 }
 
 functDemoRecursion(8);
