fe7702f7c8ec37af73ce94a15906895664e57217
Sean Luciw <131205783+seanluciw@users.noreply.github.com>
2023-05-10 13:41:29 -0700

    Update demoRecursion.js
    improved text

--- demoRecursion.js
+++ demoRecursion.js
@@ -1,21 +1,21 @@
 function functRecursion(n) {
   if (n < 1) {
-		console.log("base case reached");
+		console.log("BASE CASE reached.");
     return [];
   } else {
-		console.log("winding up layer n = " + n);
+		console.log("WINDING UP layer n = " + n);
     const arr = functRecursion(n - 1);
     arr.push(n);
-		console.log("unravelling layer n = " + n);
+		console.log("UNRAVELLING layer n = " + n);
     return arr;
   }
 }
 
 function functDemoRecursion(num) {
-	console.log("welcome to my little program showing recursion with a metaphor.");
+	console.log("Welcome to my little program showing recursion with a metaphor.");
 	arr = functRecursion(num);
-	console.log("recursion created array: " + arr);
-	console.log("this program is meant to illuminate the nature of recursion. i realized it's like winding up a spool of thread and then, after base case is reached, the spool unwinds. The array is created during the unwinding phase. \n\n")
+	console.log("RECURSION created ARRAY: " + arr);
+	console.log("This program is meant to illuminate the nature of recursion in JavaScript. I realized it's like winding up a spool of thread and then, after the base case is reached, the spool unravels. The array is created during the unravelling phase, which helps illuminate the otherwise counterintuitive ordering of the array elements. \n\n")
 }
 
 functDemoRecursion(8);
